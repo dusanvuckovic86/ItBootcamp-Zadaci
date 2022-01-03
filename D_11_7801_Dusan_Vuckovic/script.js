9c2d1eefb3b4882d8d357f1a663691e02f5c779a
dusanvuckovic86 <dusan_vuckovic@live.com>
2022-01-03 12:47:13 +0100

    domaci 11

--- D_11_7801_Dusan_Vuckovic/script.js
+++ D_11_7801_Dusan_Vuckovic/script.js
@@ -1,60 +1,76 @@
 
 
 knjiga1 = {
-    naziv: "Fric i Dobrila",
-    autor: "Srđan Valjarević",
-    slika: "<img src='asets/fric_i_dobrila-srdjan_valjarevic_v.jpg'>",
-    procitano: true
+  naziv: "Fric i Dobrila",
+  autor: "Srđan Valjarević",
+  slika: "<img src='asets/fric_i_dobrila-srdjan_valjarevic_v.jpg'>",
+  procitano: true
 }
 
 knjiga2 = {
-    naziv: "Mi protiv vas",
-    autor: "Fredrik Bakman",
-    slika: "<img src='asets/mi_protiv_vas-fredrik_bakman_v.jpg'>",
-    procitano: true
+  naziv: "Mi protiv vas",
+  autor: "Fredrik Bakman",
+  slika: "<img src='asets/mi_protiv_vas-fredrik_bakman_v.jpg'>",
+  procitano: true
 }
 
 
 knjiga3 = {
-    naziv: "Nocna škola",
-    autor: "K. Dž. Doerti",
-    slika: "<img src='asets/nocna_skola-k_dz_doerti_v.jpg'>",
-    procitano: false
+  naziv: "Nocna škola",
+  autor: "K. Dž. Doerti",
+  slika: "<img src='asets/nocna_skola-k_dz_doerti_v.jpg'>",
+  procitano: false
 }
 
 knjiga4 = {
-    naziv: "Ostrvo pacova i druge priče",
-    autor: "Ju Nesbe",
-    slika: "<img src='asets/ostrvo_pacova_i_druge_price-ju_nesbe_v.jpg'>",
-    procitano: false
+  naziv: "Ostrvo pacova i druge priče",
+  autor: "Ju Nesbe",
+  slika: "<img src='asets/ostrvo_pacova_i_druge_price-ju_nesbe_v.jpg'>",
+  procitano: false
 }
 
 
 knjiga5 = {
-    naziv: "Životinjska farma",
-    autor: "Džordž Orvel",
-    slika: "<img src='asets/zivotinjska_farma-dzordz_orvel_v.jpg'>",
-    procitano: true
+  naziv: "Životinjska farma",
+  autor: "Džordž Orvel",
+  slika: "<img src='asets/zivotinjska_farma-dzordz_orvel_v.jpg'>",
+  procitano: true
 }
 
 
 let nizKnjiga = [knjiga1, knjiga2, knjiga3, knjiga4, knjiga5];
 
 let tabela = "<table >"
-for(let i=0; i<nizKnjiga.length; i++){
-
+for (let i = 0; i < nizKnjiga.length; i++) {
+  if (i % 2 == 0) {
     let procitano = nizKnjiga[i].procitano;
-  if (procitano == true){
-    tabela += `<tr >
-    <td style='border: solid black 1px;'> ${nizKnjiga[i].slika}</td>
-    <td style='border: solid black 1px'><p style='color: blue'>${nizKnjiga[i].naziv} ${nizKnjiga[i].autor}</p></td>
+    if (procitano == true) {
+      tabela += `<tr>
+    <td style='border: solid black 1px;background-color: lightgreen'> ${nizKnjiga[i].slika}</td>
+    <td style='border: solid black 1px; background-color: lightgreen'><p style='color: blue'>${nizKnjiga[i].naziv} ${nizKnjiga[i].autor}</p></td>
     </tr>`
+    }
+    else {
+      tabela += `<tr>
+    <td style='border: solid black 1px; background-color: lightgreen'> ${nizKnjiga[i].slika}</td>
+    <td style='border: solid black 1px; background-color: lightgreen'><p style='color: gray'>${nizKnjiga[i].naziv} ${nizKnjiga[i].autor}</p></td>
+    </tr>`
+    }
   }
   else {
-    tabela += `<tr >
-    <td style='border: solid black 1px'> ${nizKnjiga[i].slika}</td>
-    <td style='border: solid black 1px'><p style='color: gray'>${nizKnjiga[i].naziv} ${nizKnjiga[i].autor}</p></td>
-    </tr>`
+    let procitano = nizKnjiga[i].procitano;
+    if (procitano == true) {
+      tabela += `<tr>
+  <td style='border: solid black 1px;'> ${nizKnjiga[i].slika}</td>
+  <td style='border: solid black 1px'><p style='color: blue'>${nizKnjiga[i].naziv} ${nizKnjiga[i].autor}</p></td>
+  </tr>`
+    }
+    else {
+      tabela += `<tr >
+  <td style='border: solid black 1px'> ${nizKnjiga[i].slika}</td>
+  <td style='border: solid black 1px'><p style='color: gray'>${nizKnjiga[i].naziv} ${nizKnjiga[i].autor}</p></td>
+  </tr>`
+    }
   }
 }
 tabela += "</table>"
