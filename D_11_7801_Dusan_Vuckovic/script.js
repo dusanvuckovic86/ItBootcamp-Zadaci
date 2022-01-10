95efcc6878e62b3c50bf5818b0ada626030137ca
dusanvuckovic86 <dusan_vuckovic@live.com>
2022-01-10 12:52:49 +0100

    promenjeno

--- D_11_7801_Dusan_Vuckovic/script.js
+++ D_11_7801_Dusan_Vuckovic/script.js
@@ -1,14 +1,16 @@
+
+
 knjiga1 = {
   naziv: "Fric i Dobrila",
   autor: "Srđan Valjarević",
-  slika: "<img src='asets/fric_i_dobrila-srdjan_valjarevic_v.jpg'>",
+  slika: 'asets/fric_i_dobrila-srdjan_valjarevic_v.jpg',
   procitano: true
 }
 
 knjiga2 = {
   naziv: "Mi protiv vas",
   autor: "Fredrik Bakman",
-  slika: "<img src='asets/mi_protiv_vas-fredrik_bakman_v.jpg'>",
+  slika: 'asets/mi_protiv_vas-fredrik_bakman_v.jpg',
   procitano: true
 }
 
@@ -16,14 +18,14 @@ knjiga2 = {
 knjiga3 = {
   naziv: "Nocna škola",
   autor: "K. Dž. Doerti",
-  slika: "<img src='asets/nocna_skola-k_dz_doerti_v.jpg'>",
+  slika: 'asets/nocna_skola-k_dz_doerti_v.jpg',
   procitano: false
 }
 
 knjiga4 = {
   naziv: "Ostrvo pacova i druge priče",
   autor: "Ju Nesbe",
-  slika: "<img src='asets/ostrvo_pacova_i_druge_price-ju_nesbe_v.jpg'>",
+  slika: 'asets/ostrvo_pacova_i_druge_price-ju_nesbe_v.jpg',
   procitano: false
 }
 
@@ -31,12 +33,13 @@ knjiga4 = {
 knjiga5 = {
   naziv: "Životinjska farma",
   autor: "Džordž Orvel",
-  slika: "<img src='asets/zivotinjska_farma-dzordz_orvel_v.jpg'>",
+  slika: 'asets/zivotinjska_farma-dzordz_orvel_v.jpg',
   procitano: true
 }
 
 
 let nizKnjiga = [knjiga1, knjiga2, knjiga3, knjiga4, knjiga5];
+
 
 let tabela = "<table >"
 for (let i = 0; i < nizKnjiga.length; i++) {
@@ -44,30 +47,28 @@ for (let i = 0; i < nizKnjiga.length; i++) {
     let procitano = nizKnjiga[i].procitano;
     if (procitano == true) {
       tabela += `<tr>
-    <td style='border: solid black 1px;background-color: lightgreen'> ${nizKnjiga[i].slika}</td>
+    <td style='border: solid black 1px;background-color: lightgreen'> <img src="${nizKnjiga[i].slika}"></td>
     <td style='border: solid black 1px; background-color: lightgreen'><p style='color: blue'>${nizKnjiga[i].naziv} ${nizKnjiga[i].autor}</p></td>
     </tr>`
     }
     else {
       tabela += `<tr>
-    <td style='border: solid black 1px; background-color: lightgreen'> ${nizKnjiga[i].slika}</td>
+    <td style='border: solid black 1px; background-color: lightgreen'> <img src="${nizKnjiga[i].slika}"></td>
     <td style='border: solid black 1px; background-color: lightgreen'><p style='color: gray'>${nizKnjiga[i].naziv} ${nizKnjiga[i].autor}</p></td>
     </tr>`
     }
   }
-
-  
   else {
     let procitano = nizKnjiga[i].procitano;
     if (procitano == true) {
       tabela += `<tr>
-  <td style='border: solid black 1px;'> ${nizKnjiga[i].slika}</td>
+  <td style='border: solid black 1px;'><img src="${nizKnjiga[i].slika}"></td>
   <td style='border: solid black 1px'><p style='color: blue'>${nizKnjiga[i].naziv} ${nizKnjiga[i].autor}</p></td>
   </tr>`
     }
     else {
       tabela += `<tr >
-  <td style='border: solid black 1px'> ${nizKnjiga[i].slika}</td>
+  <td style='border: solid black 1px'> <img src="${nizKnjiga[i].slika}"></td>
   <td style='border: solid black 1px'><p style='color: gray'>${nizKnjiga[i].naziv} ${nizKnjiga[i].autor}</p></td>
   </tr>`
     }
@@ -75,4 +76,12 @@ for (let i = 0; i < nizKnjiga.length; i++) {
 }
 tabela += "</table>"
 
-document.body.innerHTML += tabela;
+
+let bodyTag = document.getElementsByTagName("body")
+
+for(let i =0; i<bodyTag.length; i++){
+  bodyTag[i].innerHTML += tabela
+}
+
+
+
